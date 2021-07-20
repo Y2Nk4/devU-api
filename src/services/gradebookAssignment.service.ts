2db7732381b074c02fc78ac52505532a7e51faee
Emil Kovacev <emilkovacev@gmail.com>
2021-07-19 23:10:31 -0400

    added gradebook assignment service

---
+++ src/services/gradebookAssignment.service.ts
@@ -0,0 +1,36 @@
+import { getRepository, IsNull } from 'typeorm'
+
+import { GradebookAssignment } from 'devu-shared-modules'
+import GradebookAssignment from '../model/gradebookAssignments.model'
+
+const connect = () => getRepository(GradebookAssignment)
+
+export async function create(gradebookAssignment: GradebookAssignment) {
+  return await connect().save(gradebookAssignment)
+}
+
+export async function update(course: Course) {
+  const { id, name, semester, number, startDate, endDate } = course
+  if (!id) throw new Error('Missing Id')
+  return await connect().update(id, { name, semester, number, startDate, endDate })
+}
+
+export async function _delete(id: number) {
+  return await connect().softDelete({ id, deletedAt: IsNull() })
+}
+
+export async function retrieve(id: number) {
+  return await connect().findOne({ id, deletedAt: IsNull() })
+}
+
+export async function list() {
+  return await connect().find({ deletedAt: IsNull() })
+}
+
+export default {
+  create,
+  retrieve,
+  update,
+  _delete,
+  list,
+}
